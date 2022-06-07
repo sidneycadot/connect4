b41078b217159e0c50455d53d749d537bc48ea9e
Sidney Cadot <sidney@jigsaw.nl>
2022-06-07 06:06:32 +0200

    Reverted std::uint64_t to just uint64_t.

--- base62.h
+++ base62.h
@@ -16,7 +16,7 @@ char to_base62_digit(unsigned n);
 
 unsigned from_base62_digit(char c);
 
-std::string uint64_to_base62_string(std::uint64_t n, unsigned num_digits);
+std::string uint64_to_base62_string(uint64_t n, unsigned num_digits);
 
 uint64_t base62_string_to_uint64(const std::string & s);
 
