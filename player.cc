c5427c91d43cab616c0dcda6f86b82735b107973
Sidney Cadot <sidney@jigsaw.nl>
2022-06-07 06:06:56 +0200

    Introduced character constants.

--- player.cc
+++ player.cc
@@ -9,6 +9,10 @@
 
 using namespace std;
 
+const char PLAYER_A_CHAR    = 'A';
+const char PLAYER_B_CHAR    = 'B';
+const char PLAYER_NONE_CHAR = '.';
+
 istream & operator >> (istream & in, Player & player)
 {
     if (in)
@@ -18,9 +22,9 @@ istream & operator >> (istream & in, Player & player)
 
         switch (c)
         {
-            case 'A' : player = Player::A   ; break;
-            case 'B' : player = Player::B   ; break;
-            case '.' : player = Player::NONE; break;
+            case PLAYER_A_CHAR    : player = Player::A; break;
+            case PLAYER_B_CHAR    : player = Player::B; break;
+            case PLAYER_NONE_CHAR : player = Player::NONE; break;
             default : throw runtime_error("bad character for Player from input stream.");
         }
     }
@@ -31,9 +35,9 @@ ostream & operator << (ostream & out, const Player & player)
 {
     switch (player)
     {
-        case Player::A    : out << 'A'; break;
-        case Player::B    : out << 'B'; break;
-        case Player::NONE : out << '.'; break;
+        case Player::A    : out << PLAYER_A_CHAR; break;
+        case Player::B    : out << PLAYER_B_CHAR; break;
+        case Player::NONE : out << PLAYER_NONE_CHAR; break;
         default : throw runtime_error("bad Player for output stream.");
     }
     return out;
