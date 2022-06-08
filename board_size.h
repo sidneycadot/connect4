0d5493bdbbcf3ce1d256293c55788b023a74c3d7
Sidney Cadot <sidney@jigsaw.nl>
2022-06-09 00:04:46 +0200

    Calculate constants at runtime.

--- board_size.h
+++ board_size.h
@@ -6,57 +6,30 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-// These are the counts of possible legal columns in connect-4-like games,
-// when the board has a certain height V_SIZE (0..20), and the winning sequence has length Q (0..10).
-//
-//    |         0         1         2         3         4         5         6         7         8         9        10
-// ---+--------------------------------------------------------------------------------------------------------------
-//  0 |         1         1         1         1         1         1         1         1         1         1         1
-//  1 |         1         3         3         3         3         3         3         3         3         3         3
-//  2 |         1         3         7         7         7         7         7         7         7         7         7
-//  3 |         1         3        11        15        15        15        15        15        15        15        15
-//  4 |         1         3        15        27        31        31        31        31        31        31        31
-//  5 |         1         3        19        47        59        63        63        63        63        63        63
-//  6 |         1         3        23        79       111       123       127       127       127       127       127
-//  7 |         1         3        27       131       207       239       251       255       255       255       255
-//  8 |         1         3        31       215       383       463       495       507       511       511       511
-//  9 |         1         3        35       351       707       895       975      1007      1019      1023      1023
-// 10 |         1         3        39       571      1303      1727      1919      1999      2031      2043      2047
-// 11 |         1         3        43       927      2399      3331      3775      3967      4047      4079      4091
-// 12 |         1         3        47      1503      4415      6423      7423      7871      8063      8143      8175
-// 13 |         1         3        51      2435      8123     12383     14595     15615     16063     16255     16335
-// 14 |         1         3        55      3943     14943     23871     28695     30975     31999     32447     32639
-// 15 |         1         3        59      6383     27487     46015     56415     61443     63743     64767     65215
-// 16 |         1         3        63     10331     50559     88699    110911    121879    126975    129279    130303
-// 17 |         1         3        67     16719     92995    170975    218047    241759    252931    258047    260351
-// 18 |         1         3        71     27055    171047    329567    428671    479551    503831    515071    520191
-// 19 |         1         3        75     43779    314607    635263    842747    951231   1003615   1028099   1039359
-// 20 |         1         3        79     70839    578655   1224511   1656799   1886847   1999167   2052119   2076671
+#include "number_of_possible_columns.h"
 
-const int V_SIZE = 6; // Vertical board size.
-const int H_SIZE = 7; // Horizontal board size.
-const int Q      = 4; // Number of consecutive horizontal/diagonal/vertical cells required to win.
+const int V_SIZE = 4; // Vertical board size.
+const int H_SIZE = 5; // Horizontal board size.
+const int Q = 4; // Number of consecutive horizontal/diagonal/vertical cells required to win.
 
-// The number of base-62 digits needed to represent a single board state:
-//
-//   ceiling(H_SIZE * log(column_count) / log(62))
+// Find the smallest value D such that BASE^D >= COL_POS ** HORIZONTAL
 
-const unsigned NUM_BASE62_BOARD_DIGITS_v4h5q4 = 5; // 4x5 board, connect-4.
-const unsigned NUM_BASE62_BOARD_DIGITS_v5h6q4 = 6; // 5x6 board, connect-4.
-const unsigned NUM_BASE62_BOARD_DIGITS_v6h7q4 = 8; // 6x7 board, connect-4.
+constexpr uint64_t power(unsigned a, unsigned b)
+{
+    return (b == 0) ? 1 : a * power(a, b - 1);
+}
 
-const unsigned NUM_BASE62_BOARD_DIGITS = NUM_BASE62_BOARD_DIGITS_v6h7q4;
+constexpr unsigned num_digits_required(unsigned base, uint64_t value)
+{
+    return (value <= 1) ? 0 : 1 + num_digits_required(base, (value + base - 1) / base);
+}
+
+const unsigned NUMBER_OF_POSSIBLE_COLUMNS = number_of_possible_columns(Q, V_SIZE);
+const uint64_t NUMBER_OF_BOARDS_IN_COLUMN_REPRESENTATION = power(NUMBER_OF_POSSIBLE_COLUMNS, H_SIZE);
+
+const unsigned NUM_BASE62_BOARD_DIGITS  = num_digits_required(62, NUMBER_OF_BOARDS_IN_COLUMN_REPRESENTATION);
+const unsigned NUM_BASE256_BOARD_DIGITS = num_digits_required(256, NUMBER_OF_BOARDS_IN_COLUMN_REPRESENTATION);
 
 const unsigned NUM_BASE62_SCORE_DIGITS = 1; // Assume that a score can always be represented as a length-1 string.
-
-// The number of base-256 digits needed to represent a single board state:
-//
-//   ceiling(H_SIZE * log(column_count) / log(256))
-
-const unsigned NUM_BASE256_BOARD_DIGITS_v4h5q4 = 4; // 4x5 board, connect-4.
-const unsigned NUM_BASE256_BOARD_DIGITS_v5h6q4 = 5; // 5x6 board, connect-4
-const unsigned NUM_BASE256_BOARD_DIGITS_v6h7q4 = 6; // 6x7 board, connect-4
-
-const unsigned NUM_BASE256_BOARD_DIGITS = NUM_BASE256_BOARD_DIGITS_v6h7q4;
 
 #endif // CONSTANTS_H
---
+++ number_of_possible_columns.h
@@ -0,0 +1,51 @@
+
+//////////////////////////////////
+// number_of_possible_columns.h //
+//////////////////////////////////
+
+#ifndef NUMBER_OF_POSSIBLE_COLUMNS_H
+#define NUMBER_OF_POSSIBLE_COLUMNS_H
+
+// The constexpr function 'number_of_possible_columns(q, n)' calculates, possibly at runtime, the number of possible
+// connect-q columns of height up to and including n.
+
+// These are the counts of possible legal columns in connect-4-like games,
+// when the board has a certain height V_SIZE (0..20), and the winning sequence has length Q (0..10).
+//
+//    |         0         1         2         3         4         5         6         7         8         9        10
+// ---+--------------------------------------------------------------------------------------------------------------
+//  0 |         1         1         1         1         1         1         1         1         1         1         1
+//  1 |         1         3         3         3         3         3         3         3         3         3         3
+//  2 |         1         3         7         7         7         7         7         7         7         7         7
+//  3 |         1         3        11        15        15        15        15        15        15        15        15
+//  4 |         1         3        15        27        31        31        31        31        31        31        31
+//  5 |         1         3        19        47        59        63        63        63        63        63        63
+//  6 |         1         3        23        79       111       123       127       127       127       127       127
+//  7 |         1         3        27       131       207       239       251       255       255       255       255
+//  8 |         1         3        31       215       383       463       495       507       511       511       511
+//  9 |         1         3        35       351       707       895       975      1007      1019      1023      1023
+// 10 |         1         3        39       571      1303      1727      1919      1999      2031      2043      2047
+// 11 |         1         3        43       927      2399      3331      3775      3967      4047      4079      4091
+// 12 |         1         3        47      1503      4415      6423      7423      7871      8063      8143      8175
+// 13 |         1         3        51      2435      8123     12383     14595     15615     16063     16255     16335
+// 14 |         1         3        55      3943     14943     23871     28695     30975     31999     32447     32639
+// 15 |         1         3        59      6383     27487     46015     56415     61443     63743     64767     65215
+// 16 |         1         3        63     10331     50559     88699    110911    121879    126975    129279    130303
+// 17 |         1         3        67     16719     92995    170975    218047    241759    252931    258047    260351
+// 18 |         1         3        71     27055    171047    329567    428671    479551    503831    515071    520191
+// 19 |         1         3        75     43779    314607    635263    842747    951231   1003615   1028099   1039359
+// 20 |         1         3        79     70839    578655   1224511   1656799   1886847   1999167   2052119   2076671
+
+constexpr unsigned number_of_possible_columns(unsigned q, unsigned n);
+
+constexpr unsigned number_of_possible_columns_sum(unsigned q, unsigned a, unsigned b)
+{
+    return number_of_possible_columns(q, a) + ((a == b) ? 0 : number_of_possible_columns_sum(q, a + 1, b));
+}
+
+constexpr unsigned number_of_possible_columns(unsigned q, unsigned n)
+{
+    return (n <= q) ? (2 << n) - 1 : number_of_possible_columns_sum(q, n - q + 1, n - 1) + q + 2;
+}
+
+#endif // NUMBER_OF_POSSIBLE_COLUMNS_H
