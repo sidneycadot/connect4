ae38f10e12c3a04a13309926d0d541284790021d
Sidney Cadot <sidney@jigsaw.nl>
2022-07-09 11:34:10 +0200

    The script can now be run from any directory.

--- misc/show_gametree_size.py
+++ misc/show_gametree_size.py
@@ -1,8 +1,10 @@
 #! /usr/bin/env python3
 
+import sys
 import os
 
-filename_bfile = "b013582.txt"
+script_directory = os.path.dirname(sys.argv[0])
+filename_bfile = os.path.join(script_directory, "b013582.txt")
 
 total = 2266724352362
 
@@ -11,16 +13,6 @@ sum_b = 0
 with open(filename_bfile) as fi:
     for line in fi:
         (a, b) = map(int, line.split())
-
-        filename = "nodes_{}.dat.xz".format(a)
-        #try:
-        #    stat_result = os.stat(filename)
-        #    print(stat_result)
-        #except FileNotFoundError:
-        #    pass
-
         sum_b += b
         print("{:2}  {:12}  x {:8.6f} ; {:5.2f} %  {:13}  {:6.2f} %".format(a, b, b / b_prev, b / total * 100.0, sum_b, sum_b / total * 100.0))
-
         b_prev = b
-
