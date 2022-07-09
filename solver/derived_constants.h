f61122395c1bd3fc1347a7cc28b2b4ef571f09a0
Sidney Cadot <sidney@jigsaw.nl>
2022-07-09 11:23:52 +0200

    Renamed constexpr function.

--- solver/derived_constants.h
+++ solver/derived_constants.h
@@ -26,17 +26,17 @@ constexpr uint64_t power(unsigned a, unsigned b)
     return (b == 0) ? 1 : a * power(a, b - 1);
 }
 
-constexpr unsigned num_digits_required(unsigned base, uint64_t count)
+constexpr unsigned number_of_digits_required(unsigned base, uint64_t count)
 {
     // Find the smallest value 'digits' such that power(base, digits) >= count.
 
-    return (count <= 1) ? 0 : 1 + num_digits_required(base, (count + base - 1) / base);
+    return (count <= 1) ? 0 : 1 + number_of_digits_required(base, (count + base - 1) / base);
 }
 
 const unsigned NUMBER_OF_POSSIBLE_COLUMNS = number_of_possible_columns(CONNECT_Q, V_SIZE);
 const uint64_t NUMBER_OF_BOARDS_IN_COLUMN_REPRESENTATION = power(NUMBER_OF_POSSIBLE_COLUMNS, H_SIZE);
 
-const unsigned NUM_BASE62_BOARD_DIGITS  = num_digits_required( 62, NUMBER_OF_BOARDS_IN_COLUMN_REPRESENTATION);
-const unsigned NUM_BASE256_BOARD_DIGITS = num_digits_required(256, NUMBER_OF_BOARDS_IN_COLUMN_REPRESENTATION);
+const unsigned NUM_BASE62_BOARD_DIGITS  = number_of_digits_required( 62, NUMBER_OF_BOARDS_IN_COLUMN_REPRESENTATION);
+const unsigned NUM_BASE256_BOARD_DIGITS = number_of_digits_required(256, NUMBER_OF_BOARDS_IN_COLUMN_REPRESENTATION);
 
 #endif // DERIVED_CONSTANTS_H
