cb307e5363d52376d3c9d3ef5db0845d8256188a
Sidney Cadot <sidney@jigsaw.nl>
2023-03-15 18:58:54 +0100

    Finalized script and board size.

--- solver/board_size.h
+++ solver/board_size.h
@@ -8,8 +8,8 @@
 
 // These three constants define the board size and the win rule.
 
-constexpr int H_SIZE    = 7; // Horizontal board size.
-constexpr int V_SIZE    = 6; // Vertical board size.
-constexpr int CONNECT_Q = 4; // The number of connected horizontal/diagonal/vertical chips required to win.
+constexpr int H_SIZE    =  7; // Horizontal board size.
+constexpr int V_SIZE    =  6; // Vertical board size.
+constexpr int CONNECT_Q =  4; // The number of connected horizontal/diagonal/vertical chips required to win.
 
 #endif // BOARD_SIZE_H
